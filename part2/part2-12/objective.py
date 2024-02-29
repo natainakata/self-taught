9b74c529da7ea368047237594f89b1fc3ec71221
natai nakata <satzin0521@gmail.com>
2024-02-29 15:45:51 +0900

    learn jrmxlmo

--- part2/part2-12/objective.py
+++ part2/part2-12/objective.py
@@ -5,8 +5,6 @@ class Orange:
         print("Created!")
 
 
-or1 = Orange(10, "dark orange")
-or1.weight = 100
-or1.color = "light orange"
-print(or1.weight)
-print(or1.color)
+or1 = Orange(4, "dark orange")
+or2 = Orange(8, "light orange")
+or3 = Orange(14, "yellow")
